ac98d6a27a25852b7b5ade255d958a8d705084ef
OchsaufLerchenau <maciej.burdzy@gmail.com>
2020-05-23 17:34:39 +0200

    adds function to create all choice possibilities

--- Mastermind/computer-ai.rb
+++ Mastermind/computer-ai.rb
@@ -2,18 +2,34 @@ class AI
   include Operations
 
   def initialize
-    @possible_peg_sets = []
+    
   end
 
   def comp_play
   end
 
   def iterate_colors
-    p POSS_COLORS.each { |color| color }
+    POSS_COLORS.each { |color| color }
   end
 
   def create_all_possibilities
-    POSS_COLORS.each do |color|
+    possible_peg_sets = []
+    6.times do |a|
+      6.times do |b|
+        6.times do |c|
+          6. times do |d|  
+            temp_arr = []
+            temp_arr.push(
+              POSS_COLORS[a],
+              POSS_COLORS[b],
+              POSS_COLORS[c],
+              POSS_COLORS[d]
+            )
+            possible_peg_sets.push(temp_arr)
+          end
+        end
+      end
     end
+    possible_peg_sets.length
   end
 end
